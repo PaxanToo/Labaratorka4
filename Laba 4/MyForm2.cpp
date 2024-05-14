19c17fbe21f75f5b5299907897b17dea80c7b417
Захар Самойлов <tormaz557@gmail.com>
2024-05-14 21:25:39 +0300

    Реализовано хеширование

--- Laba 4/MyForm2.cpp
+++ Laba 4/MyForm2.cpp
@@ -9,17 +9,24 @@ using namespace System::Data::OleDb;
 using namespace System::Security::Cryptography;
 using namespace System::Text;
 
-// Функция для получения SHA256 хеша строки
-/*String^ GetSHA256Hash(String^ input) {
-    array<Byte>^ hashBytes = SHA256Managed::Create()->ComputeHash(Encoding::UTF8->GetBytes(input));
-    StringBuilder^ hashString = gcnew StringBuilder();
-    for each (Byte b in hashBytes) {
-        hashString->Append(b.ToString("x2"));
-    }
-    return hashString->ToString();
-}
 
-*/
+
+
+// Функция для хеширования пароля с использованием SHA-256
+String^ GenerateHash(String^ password)
+{
+    array<Byte>^ passwordBytes = Encoding::UTF8->GetBytes(password);
+
+    SHA256^ sha256 = SHA256::Create();
+    array<Byte>^ hashBytes = sha256->ComputeHash(passwordBytes);
+
+    StringBuilder^ stringBuilder = gcnew StringBuilder();
+    for each (Byte b in hashBytes) {
+        stringBuilder->Append(b.ToString("x2"));
+    }
+
+    return stringBuilder->ToString();
+}
 
 System::Void Laba4::MyForm2::button1_Click(System::Object^ sender, System::EventArgs^ e)
 {
@@ -30,21 +37,27 @@ System::Void Laba4::MyForm2::button1_Click(System::Object^ sender, System::Event
     String^ connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Database.mdb";
     OleDbConnection^ dbConnection = gcnew OleDbConnection(connectionString);
 
-    //String^ hashedPassword = GetSHA256Hash(password);
-
     try {
         dbConnection->Open();
-        
+
         String^ query = "SELECT PasswordHash FROM Пользователи WHERE Username = ?";
         OleDbCommand^ dbCommand = gcnew OleDbCommand(query, dbConnection);
-        dbCommand->Parameters->AddWithValue("?", username); 
+        dbCommand->Parameters->AddWithValue("?", username);
 
         OleDbDataReader^ dbReader = dbCommand->ExecuteReader();
 
         if (dbReader->Read()) {
-            String^ storedPassword = dbReader["PasswordHash"]->ToString();
+            // Получаем хеш пароля из базы данных
+            String^ storedPasswordHash = dbReader["PasswordHash"]->ToString();
 
-            if (storedPassword->Equals(password)) {
+            // Приводим storedPasswordHash к нижнему регистру
+            storedPasswordHash = storedPasswordHash->ToLower();
+
+            // Получаем хеш введенного пользователем пароля
+            String^ inputPasswordHash = GenerateHash(password);
+
+            // Проверяем совпадение хешей
+            if (storedPasswordHash->Equals(inputPasswordHash)) {
                 authenticated = true;
                 this->Close();
             }
